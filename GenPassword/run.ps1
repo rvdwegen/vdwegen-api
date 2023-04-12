acd5a93d33c5132593c036169e132ae517f232c5
Roel van der Wegen <github@aeternus.tech>
2023-04-12 21:33:11 +0200

    Update run.ps1

--- GenPassword/run.ps1
+++ GenPassword/run.ps1
@@ -10,12 +10,7 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
 $length = $Request.Query.length
-
-if ($Request.Query.number) {
-    $number = $Request.Query.number
-} else {
-$number = 1
-}
+if ($Request.Query.number) { $number = $Request.Query.number } else { $number = 1 }
 
 function New-Password {
     [CmdletBinding()]
