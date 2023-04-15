b9fd33753dbb07fec667b23ea44f250fe5c36619
Roel van der Wegen <github@aeternus.tech>
2023-04-15 14:32:19 +0200

    Update GraphHelper.psm1

--- GraphHelper.psm1
+++ GraphHelper.psm1
@@ -1,1 +1,56 @@
-#
+function Get-MicrosoftToken {
+    Param(
+        # Tenant Id
+        [Parameter(Mandatory=$false)]
+        [guid]$TenantId,
+
+        # Scope
+        [Parameter(Mandatory=$false)]
+        [string]$Scope = 'https://graph.microsoft.com/.default',
+
+        # ApplicationID
+        [Parameter(Mandatory=$true)]
+        [guid]$ApplicationID,
+
+        # ApplicationSecret
+        [Parameter(Mandatory=$true)]
+        [string]$ApplicationSecret,
+
+        # RefreshToken
+        [Parameter(Mandatory=$true)]
+        [string]$RefreshToken
+    )
+
+    if ($TenantId) {
+        $Uri = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
+    }
+    else {
+        $Uri = "https://login.microsoftonline.com/common/oauth2/v2.0/token"  
+    }
+
+    # Define the parameters for the token request
+    $Body = @{
+        client_id       = $ApplicationID
+        client_secret   = $ApplicationSecret
+        scope           = $Scope
+        refresh_token   = $RefreshToken
+        grant_type      = 'refresh_token'    
+    }
+
+    $Params = @{
+        Uri = $Uri
+        Method = 'POST'
+        Body = $Body
+        ContentType = 'application/x-www-form-urlencoded'
+        UseBasicParsing = $true
+    }
+
+    try {
+        $AuthResponse = (Invoke-WebRequest @Params).Content | ConvertFrom-Json
+    } catch {
+        Write-Error "Authentication Error Occured $_"
+        return
+    }
+
+    return $AuthResponse
+}
