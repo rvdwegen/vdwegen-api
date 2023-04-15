3e0fb0f0db77e5780e5fa2c96cf0d91036dcf610
Roel van der Wegen <github@aeternus.tech>
2023-04-15 16:17:29 +0200

    Update run.ps1

--- reverseLicense/run.ps1
+++ reverseLicense/run.ps1
@@ -36,5 +36,5 @@ try {
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $StatusCode
-    Body       = @($licenseResult)
+    Body       = $licenseResult
 })
