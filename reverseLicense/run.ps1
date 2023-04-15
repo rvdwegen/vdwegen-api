88259c7d71c9cf3daa403571c4b7cec6f7e92755
Roel van der Wegen <github@aeternus.tech>
2023-04-15 16:12:14 +0200

    Add files via upload

---
+++ reverseLicense/run.ps1
@@ -0,0 +1,40 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$StatusCode = [HttpStatusCode]::OK
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$license = $Request.Query.license
+
+try {
+
+    $licenseDataURL = "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv"
+    $licenseData = (Invoke-RestMethod -Method GET -Uri $licenseDataURL) | ConvertFrom-Csv | Sort-Object -Property 'GUID' -Unique
+
+    if ($license -match '(^([0-9A-Fa-f]{8}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{12})$)') {
+        $licenseResult = ($licenseData | Where-Object { $_.GUID -eq $license } | Select-Object Product_Display_Name).Product_Display_Name
+    } else {
+        $licenseResult = ($licenseData | Where-Object { $_.Product_Display_Name -eq $license } | Select-Object GUID).GUID
+    }
+
+    if (!($licenseResult)) {
+        $StatusCode = [HttpStatusCode]::BadRequest
+        $licenseResult = "No license found"
+    }
+
+} catch {
+    $ErrorMessage = $_.Exception.Message
+    $StatusCode = [HttpStatusCode]::BadRequest
+    $licenseResult = "$($ErrorMessage)"
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $StatusCode
+    Body       = @($licenseResult)
+})
