c5051e11822a6a9ca01c4159417fdc88e5cd1a19
Roel van der Wegen <github@aeternus.tech>
2023-04-16 20:01:47 +0200

    Update run.ps1

--- reverseTenant/run.ps1
+++ reverseTenant/run.ps1
@@ -18,15 +18,22 @@ Connect-AzAccount -Identity
     $context = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
     $graphToken = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.Id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, "https://graph.microsoft.com").AccessToken
 
-Connect-MgGraph -AccessToken $graphToken
+#Connect-MgGraph -AccessToken $graphToken
+
+$header = @{
+    Authorization = 'bearer {0}' -f $graphToken
+    Accept        = "application/json"
+}
 
 try {
     if ($Tenant -match '(^([0-9A-Fa-f]{8}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{12})$)') {
-        $TenantInformation = Invoke-MgGraphRequest -Method 'GET' -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByTenantId(tenantId='$Tenant')"
+        $TenantInformation = Invoke-RestMethod -Method 'GET' -Headers $header -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByTenantId(tenantId='$Tenant')"
+        #$TenantInformation = Invoke-MgGraphRequest -Method 'GET' -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByTenantId(tenantId='$Tenant')"
     } elseif ($Tenant -like "www.*" -OR $Tenant -like "http*" -OR $Tenant -like "*/*" -OR $Tenant -like "*@*") {
         throw "Please input just the domain.tld section of $Tenant"
     } else {
-        $TenantInformation = Invoke-MgGraphRequest -Method 'GET' -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByDomainName(domainName='$Tenant')" 
+        $TenantInformation = Invoke-RestMethod -Method 'GET' -Headers $header -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByDomainName(domainName='$Tenant')" 
+        #$TenantInformation = Invoke-MgGraphRequest -Method 'GET' -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByDomainName(domainName='$Tenant')" 
     }
 }
 catch {
