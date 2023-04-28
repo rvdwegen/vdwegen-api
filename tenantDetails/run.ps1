af695bbd8a7f6e40c0f7d87733d735cd788f7133
Roel van der Wegen <github@aeternus.tech>
2023-04-28 11:16:44 +0200

    Update run.ps1

--- tenantDetails/run.ps1
+++ tenantDetails/run.ps1
@@ -25,6 +25,7 @@ $header = @{
 
 try {
 
+    Write-Information "Getting basic tenant details"
     if ($Tenant -match '(^([0-9A-Fa-f]{8}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{12})$)') {
         $TenantInformation = Invoke-RestMethod -Method 'GET' -Headers $header -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByTenantId(tenantId='$Tenant')"
     } elseif ($Tenant -like "www.*" -OR $Tenant -like "http*" -OR $Tenant -like "*/*" -OR $Tenant -like "*@*") {
@@ -32,7 +33,7 @@ try {
     } else {
         $TenantInformation = Invoke-RestMethod -Method 'GET' -Headers $header -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByDomainName(domainName='$Tenant')" 
     }
-
+    Write-Information "after Getting basic tenant details"
     $domainsBody = "<?xml version=`"1.0`" encoding=`"utf-8`"?><soap:Envelope xmlns:exm=`"http://schemas.microsoft.com/exchange/services/2006/messages`" xmlns:ext=`"http://schemas.microsoft.com/exchange/services/2006/types`" xmlns:a=`"http://www.w3.org/2005/08/addressing`" xmlns:soap=`"http://schemas.xmlsoap.org/soap/envelope/`" xmlns:xsi=`"http://www.w3.org/2001/XMLSchema-instance`" xmlns:xsd=`"http://www.w3.org/2001/XMLSchema`"><soap:Header><a:Action soap:mustUnderstand=`"1`">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action><a:To soap:mustUnderstand=`"1`">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To><a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo></soap:Header><soap:Body><GetFederationInformationRequestMessage xmlns=`"http://schemas.microsoft.com/exchange/2010/Autodiscover`"><Request><Domain>$Tenant</Domain></Request></GetFederationInformationRequestMessage></soap:Body></soap:Envelope>"
 
     # Create the headers
@@ -56,8 +57,7 @@ try {
 
 }
 catch {
-Write-Host $_
-Write-Error $_
+    Write-Error $_
     $ErrorMessage = $_.Exception.Message
     $StatusCode = [HttpStatusCode]::BadRequest
     $fullDetails = "Error: $($ErrorMessage)"
