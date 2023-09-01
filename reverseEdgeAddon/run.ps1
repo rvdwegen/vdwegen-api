86cfffece0e7477883784435d234e8b8ed440de6
Roel van der Wegen <github@aeternus.tech>
2023-09-01 08:22:09 +0200

    .

---
+++ reverseEdgeAddon/run.ps1
@@ -0,0 +1,30 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$APIName = $TriggerMetadata.FunctionName
+$StatusCode = [HttpStatusCode]::OK
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$addon = $Request.Query.addon
+
+try {
+
+$addonDetails = Invoke-RestMethod -Method GET -Uri "https://microsoftedge.microsoft.com/addons/getproductdetailsbycrxid/$($addon)" | Select-Object name,shortDescription,thumbnail
+
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    $StatusCode = [HttpStatusCode]::BadRequest
+    $addonDetails = "$($ErrorMessage)"
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $StatusCode
+    Body       = @($addonDetails)
+})
