26634fcd9102330540c9efab8e1665e5ea596d67
Roel van der Wegen <github@aeternus.tech>
2023-12-13 19:24:06 +0100

    .

---
+++ myip/run.ps1
@@ -0,0 +1,12 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$StatusCode = [HttpStatusCode]::OK
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $StatusCode
+    Body       = ($TriggerMetaData.Headers.'CLIENT-IP').Split(':')[0]
+})
