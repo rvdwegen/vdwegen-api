45ffdb98205f8511a4bead8fae4981c32713a516
Roel van der Wegen <github@aeternus.tech>
2023-12-14 20:31:08 +0100

    .

--- hookTest/run.ps1
+++ hookTest/run.ps1
@@ -5,8 +5,18 @@ param($Request, $TriggerMetadata)
 
 $StatusCode = [HttpStatusCode]::OK
 
+if ($Request.Query.validationToken) {
+    $response = $Request.Query.validationToken
+} else {
+    Write-Host "####################################"
+    Write-Host "####################################"
+    Write-Host $Request
+    Write-Host "####################################"
+    Write-Host "####################################"
+}
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $StatusCode
-    Body       = $Request
+    Body       = $response
 })
