079d4df165247873924ccf67f5051f126dac0a8c
Roel van der Wegen <github@aeternus.tech>
2024-02-14 23:58:15 +0100

    .

--- redirect/run.ps1
+++ redirect/run.ps1
@@ -3,19 +3,21 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+$randomSlug = (("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789").ToCharArray() | Get-Random -Count 8) -Join ""
+
+
 try {
     Connect-AzAccount -Identity
-
-    $urlTableContext = New-AzDataTableContext -TableName 'shorturls' -StorageAccountName 'stourlshort' -ManagedIdentity
-
-    $urlObject = (Get-AzDataTableEntity -Filter "RowKey eq '$($Request.Query.code)'" -context $urlTableContext)
-
 } catch {
-    $_.Exception.Message
     $_.Exception.Message
     $ErrorMessage = $_.Exception.Message
     $StatusCode = [HttpStatusCode]::Unauthorized
 }
+
+$urlTableContext = New-AzDataTableContext -TableName 'shorturls' -StorageAccountName 'stourlshort' -ManagedIdentity
+
+$urlObject = (Get-AzDataTableEntity -Filter "RowKey eq '$($Request.Query.code)'" -context $urlTableContext)
+
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
