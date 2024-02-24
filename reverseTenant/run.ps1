80942eb05fcdbd09020eaf1650f569b5550d22a0
Roel van der Wegen <github@aeternus.tech>
2024-02-24 21:19:13 +0100

    .

--- reverseTenant/run.ps1
+++ reverseTenant/run.ps1
@@ -45,5 +45,5 @@ catch {
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $StatusCode
-    Body       = @($TenantInformation)
+    Body       = $TenantInformation
 })
