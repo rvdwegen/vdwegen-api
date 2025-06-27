b74b4f813e9bf0114c0e655359a1d549dc92e512
Roel van der Wegen <github@aeternus.tech>
2025-06-27 13:32:57 +0100

    test

--- myip/run.ps1
+++ myip/run.ps1
@@ -5,6 +5,10 @@ param($Request, $TriggerMetadata)
 
 $StatusCode = [HttpStatusCode]::OK
 
+$env:WEBSITE_HOSTNAME
+$env:WEBSITE_HOSTNAME
+$env:WEBSITE_HOSTNAME
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $StatusCode
